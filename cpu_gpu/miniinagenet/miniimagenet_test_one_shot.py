d20754d4c8592250a1f3673ddf6a085100c7f104
gojay <gao.jay@foxmail.com>
2019-07-24 15:48:18 +0800

    provide the version which can running in both cpu and gpu

--- cpu_gpu/miniinagenet/miniimagenet_test_one_shot.py
+++ cpu_gpu/miniinagenet/miniimagenet_test_one_shot.py
@@ -12,11 +12,10 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.optim.lr_scheduler import StepLR
 import numpy as np
-import task_generator as tg
+import task_generator_test as tg
 import os
 import math
 import argparse
-import random
 import scipy as sp
 import scipy.stats
 
@@ -25,9 +24,9 @@ parser.add_argument("-f","--feature_dim",type = int, default = 64)
 parser.add_argument("-r","--relation_dim",type = int, default = 8)
 parser.add_argument("-w","--class_num",type = int, default = 5)
 parser.add_argument("-s","--sample_num_per_class",type = int, default = 1)
-parser.add_argument("-b","--batch_num_per_class",type = int, default = 19)
+parser.add_argument("-b","--batch_num_per_class",type = int, default = 10)
 parser.add_argument("-e","--episode",type = int, default= 10)
-parser.add_argument("-t","--test_episode", type = int, default = 1000)
+parser.add_argument("-t","--test_episode", type = int, default = 600)
 parser.add_argument("-l","--learning_rate", type = float, default = 0.001)
 parser.add_argument("-g","--gpu",type=int, default=0)
 parser.add_argument("-u","--hidden_unit",type=int,default=10)
@@ -50,16 +49,17 @@ device = torch.device("cuda:"+str(GPU) if torch.cuda.is_available() else "cpu")
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0*np.array(data)
     n = len(a)
-    m, se = np.mean(a),scipy.stats.sem(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
     h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
     return m,h
+
 
 class CNNEncoder(nn.Module):
     """docstring for ClassName"""
     def __init__(self):
         super(CNNEncoder, self).__init__()
         self.layer1 = nn.Sequential(
-                        nn.Conv2d(1,64,kernel_size=3,padding=0),
+                        nn.Conv2d(3,64,kernel_size=3,padding=0),
                         nn.BatchNorm2d(64, momentum=1, affine=True),
                         nn.ReLU(),
                         nn.MaxPool2d(2))
@@ -90,16 +90,16 @@ class RelationNetwork(nn.Module):
     def __init__(self,input_size,hidden_size):
         super(RelationNetwork, self).__init__()
         self.layer1 = nn.Sequential(
-                        nn.Conv2d(128,64,kernel_size=3,padding=1),
+                        nn.Conv2d(128,64,kernel_size=3,padding=0),
                         nn.BatchNorm2d(64, momentum=1, affine=True),
                         nn.ReLU(),
                         nn.MaxPool2d(2))
         self.layer2 = nn.Sequential(
-                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.Conv2d(64,64,kernel_size=3,padding=0),
                         nn.BatchNorm2d(64, momentum=1, affine=True),
                         nn.ReLU(),
                         nn.MaxPool2d(2))
-        self.fc1 = nn.Linear(input_size,hidden_size)
+        self.fc1 = nn.Linear(input_size*3*3,hidden_size)
         self.fc2 = nn.Linear(hidden_size,1)
 
     def forward(self,x):
@@ -129,7 +129,7 @@ def main():
     # Step 1: init data folders
     print("init data folders")
     # init character folders for dataset construction
-    metatrain_character_folders,metatest_character_folders = tg.omniglot_character_folders()
+    metatrain_folders,metatest_folders = tg.mini_imagenet_folders()
 
     # Step 2: init neural networks
     print("init neural networks")
@@ -137,69 +137,69 @@ def main():
     feature_encoder = CNNEncoder()
     relation_network = RelationNetwork(FEATURE_DIM,RELATION_DIM)
 
+
     feature_encoder.to(device)
     relation_network.to(device)
 
-    feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(),lr=LEARNING_RATE)
-    feature_encoder_scheduler = StepLR(feature_encoder_optim,step_size=100000,gamma=0.5)
-    relation_network_optim = torch.optim.Adam(relation_network.parameters(),lr=LEARNING_RATE)
-    relation_network_scheduler = StepLR(relation_network_optim,step_size=100000,gamma=0.5)
 
-    if os.path.exists(str("./models/omniglot_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
-        feature_encoder.load_state_dict(torch.load(str("./models/omniglot_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"), map_location=device))
+    if os.path.exists(str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
+        feature_encoder.load_state_dict(torch.load(str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"), map_location=device))
         print("load feature encoder success")
-    if os.path.exists(str("./models/omniglot_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
-        relation_network.load_state_dict(torch.load(str("./models/omniglot_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"), map_location=device))
+    if os.path.exists(str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
+        relation_network.load_state_dict(torch.load(str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"), map_location=device))
         print("load relation network success")
+
+    # Step 3: build graph
 
     total_accuracy = 0.0
     for episode in range(EPISODE):
 
-
             # test
             print("Testing...")
-            total_rewards = 0
+
             accuracies = []
             for i in range(TEST_EPISODE):
-                degrees = random.choice([0,90,180,270])
-                task = tg.OmniglotTask(metatest_character_folders,CLASS_NUM,SAMPLE_NUM_PER_CLASS,SAMPLE_NUM_PER_CLASS,)
-                sample_dataloader = tg.get_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False,rotation=degrees)
-                test_dataloader = tg.get_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="test",shuffle=True,rotation=degrees)
+                total_rewards = 0
+                counter = 0
+                task = tg.MiniImagenetTask(metatest_folders,CLASS_NUM,1,15)
+                sample_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=1,split="train",shuffle=False)
 
+                num_per_class = 3
+                test_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=num_per_class,split="test",shuffle=True)
                 sample_images,sample_labels = sample_dataloader.__iter__().next()
-                test_images,test_labels = test_dataloader.__iter__().next()
+                for test_images,test_labels in test_dataloader:
+                    batch_size = test_labels.shape[0]
+                    # calculate features
+                    sample_features = feature_encoder(Variable(sample_images).to(device)) # 5x64
+                    test_features = feature_encoder(Variable(test_images).to(device)) # 20x64
 
-                # calculate features
-                sample_features = feature_encoder(Variable(sample_images).to(device)) # 5x64
-                test_features = feature_encoder(Variable(test_images).to(device)) # 20x64
+                    # calculate relations
+                    # each batch sample link to every samples to calculate relations
+                    # to form a 100x128 matrix for relation network
+                    sample_features_ext = sample_features.unsqueeze(0).repeat(batch_size,1,1,1,1)
+                    test_features_ext = test_features.unsqueeze(0).repeat(1*CLASS_NUM,1,1,1,1)
+                    test_features_ext = torch.transpose(test_features_ext,0,1)
+                    relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,19,19)
+                    relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
 
-                # calculate relations
-                # each batch sample link to every samples to calculate relations
-                # to form a 100x128 matrix for relation network
-                sample_features_ext = sample_features.unsqueeze(0).repeat(SAMPLE_NUM_PER_CLASS*CLASS_NUM,1,1,1,1)
-                test_features_ext = test_features.unsqueeze(0).repeat(SAMPLE_NUM_PER_CLASS*CLASS_NUM,1,1,1,1)
-                test_features_ext = torch.transpose(test_features_ext,0,1)
+                    _,predict_labels = torch.max(relations.data,1)
+                    predict_labels = predict_labels.to(device,dtype=torch.int32)
+                    test_labels = test_labels.to(device,dtype=torch.int32)
 
-                relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,5,5)
-                relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
+                    rewards = [1 if predict_labels[j]==test_labels[j] else 0 for j in range(batch_size)]
 
-                _,predict_labels = torch.max(relations.data,1)
-                predict_labels = predict_labels.to(device,dtype=torch.int32)
-                test_labels = test_labels.to(device,dtype=torch.int32)
-
-                rewards = [1 if predict_labels[j]==test_labels[j] else 0 for j in range(CLASS_NUM)]
-
-                total_rewards += np.sum(rewards)
-                accuracy = np.sum(rewards)/1.0/CLASS_NUM/SAMPLE_NUM_PER_CLASS
+                    total_rewards += np.sum(rewards)
+                    counter += batch_size
+                accuracy = total_rewards/1.0/counter
                 accuracies.append(accuracy)
 
             test_accuracy,h = mean_confidence_interval(accuracies)
 
             print("test accuracy:",test_accuracy,"h:",h)
+
             total_accuracy += test_accuracy
 
     print("aver_accuracy:",total_accuracy/EPISODE)
-
 
 
 
